2cfed6566beea92d8a7ef18d6561c4dd9805f5f4
Yohannah Véras <yohannahlv@gmail.com>
2023-06-10 16:53:18 -0300

    first commit

---
+++ soma_vetor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int n;
+            double soma, media;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+                      
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            Console.WriteLine();
+            Console.Write("VALORES = ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(vet[i].ToString("F1", CI) + " ");
+            }
+
+            soma = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                soma = soma + vet[i];
+            }
+
+            media = soma / n;
+
+            Console.WriteLine();
+            Console.WriteLine("SOMA = " + soma.ToString("F2", CI));
+            Console.WriteLine("MEDIA = " + media.ToString("F2", CI));
+
+
+
+
+
+
+        }
+    }
+}
